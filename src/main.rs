dc8d35373ed150f13773d0a7765773d3e6f763e6
yajamon <yajamon.tatsuki@gmail.com>
2018-02-16 18:33:14 +0900

    試しにclapを動かす

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new("zacli")
+        .version("0.1.0")
+        .about("Zaif api client for command line interface")
+        .author("yajamon <yajamon.tatsuki@gmail.com>")
+        .get_matches();
 }

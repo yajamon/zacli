e0987bab73f1a27a9b3c18fe90bc43ddf8a8ffc1
yajamon <yajamon.tatsuki@gmail.com>
2018-02-16 20:43:59 +0900

    currenciesのサブコマンドを実装

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 #[macro_use]
 extern crate clap;
-use clap::App;
+extern crate zaif_api;
+
+use clap::{Arg, SubCommand};
+use zaif_api::public_api::*;
 
 fn main() {
-    app_from_crate!().get_matches();
+    let app = app_from_crate!().subcommand(
+        SubCommand::with_name("currencies")
+            .about("通貨情報を取得します")
+            .arg(Arg::with_name("NAME").help("通貨の名前")),
+    );
+
+    let matches = app.get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("currencies") {
+        let api = CurrenciesBuilder::new()
+            .name(matches.value_of("NAME").unwrap_or("all").to_string())
+            .finalize();
+        for currency in api.exec().unwrap() {
+            println!("{}", currency.name);
+        }
+    }
 }

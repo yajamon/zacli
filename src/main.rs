f53c47116c23a115a8eee8b6f0ebc79e26b8874f
yajamon <yajamon.tatsuki@gmail.com>
2018-02-17 23:55:09 +0900

    Depthを実装

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ use clap::{Arg, SubCommand};
 use zaif_api::public_api::*;
 
 fn main() {
-    let app = app_from_crate!().subcommand(
-        SubCommand::with_name("currencies")
-            .about("通貨情報を取得します")
-            .arg(Arg::with_name("NAME").help("通貨の名前")),
-    );
+    let app = app_from_crate!()
+        .subcommand(
+            SubCommand::with_name("currencies")
+                .about("通貨情報を取得します")
+                .arg(Arg::with_name("NAME").help("通貨の名前")),
+        )
+        .subcommand(
+            SubCommand::with_name("depth")
+                .about("板情報を取得します")
+                .arg(Arg::with_name("CURRENCY_PAIR"))
+                .help("取引通貨の組み合わせ"),
+        );
 
     let matches = app.get_matches();
 
@@ -20,6 +27,15 @@ fn main() {
             .finalize();
         for currency in api.exec().unwrap() {
             println!("{}", currency.name);
+        }
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("depth") {
+        let api = DepthBuilder::new()
+            .currency_pair(matches.value_of("CURRENCY_PAIR").unwrap().to_string())
+            .finalize();
+        for ask in api.exec().unwrap().asks {
+            println!("ask price: {}, amount: {}", ask.price(), ask.amount());
         }
     }
 }

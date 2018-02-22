fa5131ee668b05c74d16920fe6dc23107686e82a
yajamon <yajamon.tatsuki@gmail.com>
2018-02-22 22:23:03 +0900

    コマンド名の定数化: currencies

--- src/commands/currencies.rs
+++ src/commands/currencies.rs
@@ -3,11 +3,12 @@ use clap::{App, Arg, SubCommand};
 
 use commands::Define;
 
+const COMMAND_NAME: &str = "currencies";
 pub struct Command;
 
 impl Define for Command {
     fn define<'a, 'b>() -> App<'a, 'b> {
-        SubCommand::with_name("currencies")
+        SubCommand::with_name(COMMAND_NAME)
             .about("通貨情報を取得します")
             .arg(Arg::with_name("NAME").help("通貨の名前"))
     }

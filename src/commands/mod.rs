d55877d57940127082a4f1296eb65ebc79c1fc59
yajamon <yajamon.tatsuki@gmail.com>
2018-02-22 22:24:25 +0900

    マッチした要素適応をdispatch()に隔離

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,7 @@
 extern crate clap;
-use clap::{App, Arg, SubCommand};
+extern crate zaif_api;
+use clap::{App, Arg, ArgMatches};
+use self::zaif_api::public_api::*;
 
 pub mod currencies;
 pub mod depth;
@@ -11,4 +13,24 @@ pub trait Define {
 pub fn define_subcommands<'a, 'b>(cmd: App<'a, 'b>) -> App<'a, 'b> {
     cmd.subcommand(currencies::Command::define())
         .subcommand(depth::Command::define())
+}
+
+pub fn dispatch<'a>(matches: ArgMatches<'a>) {
+    if let Some(ref matches) = matches.subcommand_matches("currencies") {
+        let api = CurrenciesBuilder::new()
+            .name(matches.value_of("NAME").unwrap_or("all").to_string())
+            .finalize();
+        for currency in api.exec().unwrap() {
+            println!("{}", currency.name);
+        }
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("depth") {
+        let api = DepthBuilder::new()
+            .currency_pair(matches.value_of("CURRENCY_PAIR").unwrap().to_string())
+            .finalize();
+        for ask in api.exec().unwrap().asks {
+            println!("ask price: {}, amount: {}", ask.price(), ask.amount());
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 #[macro_use]
 extern crate clap;
-extern crate zaif_api;
-
-use zaif_api::public_api::*;
 
 mod commands;
 
@@ -11,21 +8,5 @@ fn main() {
 
     let matches = app.get_matches();
 
-    if let Some(ref matches) = matches.subcommand_matches("currencies") {
-        let api = CurrenciesBuilder::new()
-            .name(matches.value_of("NAME").unwrap_or("all").to_string())
-            .finalize();
-        for currency in api.exec().unwrap() {
-            println!("{}", currency.name);
-        }
-    }
-
-    if let Some(ref matches) = matches.subcommand_matches("depth") {
-        let api = DepthBuilder::new()
-            .currency_pair(matches.value_of("CURRENCY_PAIR").unwrap().to_string())
-            .finalize();
-        for ask in api.exec().unwrap().asks {
-            println!("ask price: {}, amount: {}", ask.price(), ask.amount());
-        }
-    }
+    commands::dispatch(matches);
 }

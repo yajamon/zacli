ca8ef7164b088e53b3f841f0e7675ebe5bb78209
yajamon <yajamon.tatsuki@gmail.com>
2018-02-27 15:19:53 +0900

    SubCommandを生成するscriptを追加
    コピー&ペーストで頑張るくらいならスクリプトファイルのほうがいい

---
+++ scripts/add_command.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+readonly ROOT_DIR=$(cd $(dirname $(dirname $BASH_SOURCE)); pwd)
+readonly COMMANDS_DIR="$ROOT_DIR/src/commands"
+readonly COMMAND_NAME=$1
+
+if [ -z $COMMAND_NAME ]; then
+    echo 'Require command name' >&2
+    exit 1
+fi
+
+cat << EOT
+extern crate clap;
+extern crate zaif_api;
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+use self::zaif_api::public_api::${COMMAND_NAME}Builder;
+
+use commands::{Define, Run};
+
+pub const COMMAND_NAME: &str = "${COMMAND_NAME}";
+pub struct Command;
+
+impl Define for Command {
+    fn define<'a, 'b>() -> App<'a, 'b> {
+        SubCommand::with_name(COMMAND_NAME)
+            .about("command description")
+            .arg(Arg::with_name("Argument_name"))
+            .help("command help")
+    }
+}
+
+impl Run for Command {
+    fn name<'a>() -> &'a str {
+        COMMAND_NAME
+    }
+
+    fn run<'a>(matches: &ArgMatches<'a>) {
+        let api = ${COMMAND_NAME}Builder::new()
+            .finalize();
+        let result = api.exec().unwrap();
+        println!("{}", "result...");
+    }
+}
+EOT
